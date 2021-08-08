db062801bbb7015d5cf48c6df8ca0792ba769772
max Icolumi <max77648@gmail.com>
2021-08-08 14:42:08 +0300

    Добавил удаление

--- Replacer/StartMenu.Designer.cs
+++ Replacer/StartMenu.Designer.cs
@@ -29,39 +29,43 @@ namespace Replacer
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.textBoxInputReplace = new System.Windows.Forms.TabControl();
             this.Delete = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxChanges = new System.Windows.Forms.ListBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxInputDelete = new System.Windows.Forms.TextBox();
             this.Replace = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBoxShowPath = new System.Windows.Forms.TextBox();
             this.buttonBrowser = new System.Windows.Forms.Button();
-            this.textBoxInputDelete = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.buttonDelete = new System.Windows.Forms.Button();
-            this.tabControl1.SuspendLayout();
+            this.textBoxInputReplace.SuspendLayout();
             this.Delete.SuspendLayout();
             this.Replace.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // textBoxInputReplace
             // 
-            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.textBoxInputReplace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl1.Controls.Add(this.Delete);
-            this.tabControl1.Controls.Add(this.Replace);
-            this.tabControl1.Location = new System.Drawing.Point(12, 45);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(720, 458);
-            this.tabControl1.TabIndex = 0;
+            this.textBoxInputReplace.Controls.Add(this.Delete);
+            this.textBoxInputReplace.Controls.Add(this.Replace);
+            this.textBoxInputReplace.Location = new System.Drawing.Point(12, 45);
+            this.textBoxInputReplace.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxInputReplace.Name = "textBoxInputReplace";
+            this.textBoxInputReplace.SelectedIndex = 0;
+            this.textBoxInputReplace.Size = new System.Drawing.Size(719, 415);
+            this.textBoxInputReplace.TabIndex = 0;
             // 
             // Delete
             // 
             this.Delete.BackColor = System.Drawing.Color.AliceBlue;
+            this.Delete.Controls.Add(this.label4);
+            this.Delete.Controls.Add(this.listBoxChanges);
             this.Delete.Controls.Add(this.buttonDelete);
             this.Delete.Controls.Add(this.label2);
             this.Delete.Controls.Add(this.textBoxInputDelete);
@@ -69,10 +73,66 @@ namespace Replacer
             this.Delete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Delete.Name = "Delete";
             this.Delete.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Delete.Size = new System.Drawing.Size(712, 427);
+            this.Delete.Size = new System.Drawing.Size(711, 384);
             this.Delete.TabIndex = 0;
             this.Delete.Text = "Удаление";
             this.Delete.ToolTipText = "Удаление введенных слов, словосочетаний, ссылок во всех тхт, что есть в папке.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(427, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Изменения в файлах:";
+            // 
+            // listBoxChanges
+            // 
+            this.listBoxChanges.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxChanges.FormattingEnabled = true;
+            this.listBoxChanges.HorizontalScrollbar = true;
+            this.listBoxChanges.ItemHeight = 15;
+            this.listBoxChanges.Location = new System.Drawing.Point(430, 27);
+            this.listBoxChanges.Name = "listBoxChanges";
+            this.listBoxChanges.ScrollAlwaysVisible = true;
+            this.listBoxChanges.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBoxChanges.Size = new System.Drawing.Size(275, 304);
+            this.listBoxChanges.Sorted = true;
+            this.listBoxChanges.TabIndex = 3;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDelete.AutoSize = true;
+            this.buttonDelete.BackColor = System.Drawing.Color.White;
+            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDelete.Location = new System.Drawing.Point(629, 346);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(76, 30);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(416, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите слова/словосочетания/ссылки(через запятую):";
+            // 
+            // textBoxInputDelete
+            // 
+            this.textBoxInputDelete.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxInputDelete.Location = new System.Drawing.Point(4, 27);
+            this.textBoxInputDelete.Multiline = true;
+            this.textBoxInputDelete.Name = "textBoxInputDelete";
+            this.textBoxInputDelete.Size = new System.Drawing.Size(420, 353);
+            this.textBoxInputDelete.TabIndex = 0;
+            this.textBoxInputDelete.Text = "Учёба,Привет мир,пройдет,Рождественская коллекция";
             // 
             // Replace
             // 
@@ -83,11 +143,20 @@ namespace Replacer
             this.Replace.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Replace.Name = "Replace";
             this.Replace.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Replace.Size = new System.Drawing.Size(705, 387);
+            this.Replace.Size = new System.Drawing.Size(712, 427);
             this.Replace.TabIndex = 1;
             this.Replace.Text = "Замена";
             this.Replace.ToolTipText = "Замена, ввести одно слово\\словосочетание и на что заменить, так же во всех файлах" +
     " тхт, что есть в папке.";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(4, 27);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(314, 353);
+            this.textBox2.TabIndex = 2;
             // 
             // textBoxShowPath
             // 
@@ -96,9 +165,9 @@ namespace Replacer
             this.textBoxShowPath.BackColor = System.Drawing.Color.AliceBlue;
             this.textBoxShowPath.Location = new System.Drawing.Point(123, 12);
             this.textBoxShowPath.Name = "textBoxShowPath";
+            this.textBoxShowPath.ReadOnly = true;
             this.textBoxShowPath.Size = new System.Drawing.Size(528, 25);
             this.textBoxShowPath.TabIndex = 1;
-            this.textBoxShowPath.Text = "C:\\Users\\Admin\\Desktop\\1";
             this.textBoxShowPath.TextChanged += new System.EventHandler(this.textBoxShowPath_TextChanged);
             // 
             // buttonBrowser
@@ -114,16 +183,6 @@ namespace Replacer
             this.buttonBrowser.UseVisualStyleBackColor = false;
             this.buttonBrowser.Click += new System.EventHandler(this.buttonBrowser_Click);
             // 
-            // textBoxInputDelete
-            // 
-            this.textBoxInputDelete.BackColor = System.Drawing.SystemColors.Info;
-            this.textBoxInputDelete.Location = new System.Drawing.Point(4, 27);
-            this.textBoxInputDelete.Multiline = true;
-            this.textBoxInputDelete.Name = "textBoxInputDelete";
-            this.textBoxInputDelete.Size = new System.Drawing.Size(314, 353);
-            this.textBoxInputDelete.TabIndex = 0;
-            this.textBoxInputDelete.Text = "Привет, учёба, привет мир";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -133,46 +192,14 @@ namespace Replacer
             this.label1.TabIndex = 3;
             this.label1.Text = "Путь к папке:";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 6);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(296, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Введите слова/словосочетания/ссылки:";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(4, 6);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(296, 18);
+            this.label3.Size = new System.Drawing.Size(416, 18);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Введите слова/словосочетания/ссылки:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
-            this.textBox2.Location = new System.Drawing.Point(4, 27);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(314, 353);
-            this.textBox2.TabIndex = 2;
-            // 
-            // buttonDelete
-            // 
-            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonDelete.AutoSize = true;
-            this.buttonDelete.BackColor = System.Drawing.Color.White;
-            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonDelete.Location = new System.Drawing.Point(629, 389);
-            this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(76, 30);
-            this.buttonDelete.TabIndex = 2;
-            this.buttonDelete.Text = "Удалить";
-            this.buttonDelete.UseVisualStyleBackColor = false;
-            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            this.label3.Text = "Введите слова/словосочетания/ссылки(через запятую):";
             // 
             // StartMenu
             // 
@@ -183,14 +210,14 @@ namespace Replacer
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonBrowser);
             this.Controls.Add(this.textBoxShowPath);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBoxInputReplace);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "StartMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Replacer";
-            this.tabControl1.ResumeLayout(false);
+            this.textBoxInputReplace.ResumeLayout(false);
             this.Delete.ResumeLayout(false);
             this.Delete.PerformLayout();
             this.Replace.ResumeLayout(false);
@@ -202,7 +229,7 @@ namespace Replacer
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl textBoxInputReplace;
         private System.Windows.Forms.TabPage Delete;
         private System.Windows.Forms.TabPage Replace;
         private System.Windows.Forms.TextBox textBoxShowPath;
@@ -210,9 +237,11 @@ namespace Replacer
         private System.Windows.Forms.TextBox textBoxInputDelete;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBoxChanges;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Replacer/StartMenu.cs
+++ Replacer/StartMenu.cs
@@ -13,20 +13,63 @@ namespace Replacer
 {
     public partial class StartMenu : Form
     {
-        private string _path = string.Empty;
-        private List
+        private string _path;
+        private List<string> _files;
+        private List<string> _fileNames;
+
         public StartMenu()
         {
+            _path = string.Empty;
+            _files = new List<string>();
+            _fileNames = new List<string>();
             InitializeComponent();
         }
 
-        private void buttonBrowser_Click(object sender, EventArgs e)
+        private void Initialize()
         {
-            SetPath();
-            textBoxShowPath.Text = _path;
+            _path = GetDirectory();
+            if (_path != string.Empty)
+            {
+                SetFiles();
+                textBoxShowPath.Text =  _path;
+                SetNameFiles();
+            }
         }
 
-        private void SetPath()
+        private void ClearVariable()
+        {
+            _path = string.Empty;
+            _files.Clear();
+            _fileNames.Clear();
+        }
+
+        private void ClearInterface()
+        {
+            textBoxShowPath.Clear();
+            textBoxInputDelete.Clear();
+            listBoxChanges.Items.Clear();
+        }
+
+        private void SetNameFiles()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(_path);
+            var files = directoryInfo.GetFiles("*.txt");
+            foreach (var file in files)
+            {
+                _fileNames.Add(file.Name);
+            }
+        }
+
+        private void SetFiles()
+        {
+            _files = Directory.GetFiles(_path, "*.txt").ToList<string>();
+            if (_files.Count == 0)
+            {
+                MessageBox.Show($"Файлов с расширением txt в папке {_path} - нет.");
+            }
+        }
+
+        private string GetDirectory()
         {
             FolderBrowserDialog browserDialog = new FolderBrowserDialog();
             browserDialog.Description = "Выберете папку";
@@ -34,21 +77,88 @@ namespace Replacer
             browserDialog.ShowNewFolderButton = true;
             if (browserDialog.ShowDialog() == DialogResult.OK)
             {
-                _path = browserDialog.SelectedPath;
+                return browserDialog.SelectedPath;
+            }
+            else
+            {
+                return string.Empty;
             }
         }
 
-        private void buttonDelete_Click(object sender, EventArgs e)
+        private void ReplaceFiles(List<string> words)
         {
-            if (_path != string.Empty && Directory.Exists(_path))
+            listBoxChanges.Items.Clear();
+            bool isContains = false;
+            for (int i = 0; i < _files.Count; i++)
             {
-                List<string> lines = textBoxInputDelete.Text.Split(',').ToList<string>();
-                lines.Remove("");
+                string text = File.ReadAllText(_files[i]);
+                if (text != string.Empty)
+                {
+                    foreach (var word in words)
+                    {
+                        if (text.Contains(word))
+                        {
+                            text = text.Replace(word, string.Empty);
+                            isContains = true;
+                        }
+                    }
+                    if (isContains)
+                    {
+                        while (text.Contains("  "))
+                        {
+                            text = text.Replace("  ", " ");
+                        }
+                        File.Delete(_files[i]);
+                        using (StreamWriter stream = new StreamWriter(File.OpenWrite(_files[i])))
+                        {
+                            stream.WriteAsync(text).Wait();
+                        }
+                        
+                        listBoxChanges.Items.Add(_fileNames[i]);
+                        isContains = false;
+                    }
+                }
+            }
+        }
+
+        private void SplitAndReplace()
+        {
+            if (_path != string.Empty && _files.Count != 0)
+            {
+                List<string> words = textBoxInputDelete.Text.Split(',').ToList<string>();
+                words.Remove("");
+                if (textBoxInputReplace.SelectedTab.Text == "Удаление")
+                {
+                    ReplaceFiles(words);
+                }
+                else if (textBoxInputReplace.SelectedTab.Text == "Замена")
+                {
+                    ReplaceFiles(words);
+                }
+                else
+                {
+                    throw new Exception("Check tabContol");
+                }
+            }
+            else if (Directory.Exists(_path) == false)
+            {
+                MessageBox.Show("Папки по такому пути - нет");
             }
             else
             {
                 MessageBox.Show("Выберете папку с файлами");
             }
+        }
+
+        private void buttonBrowser_Click(object sender, EventArgs e)
+        {
+            ClearVariable();
+            Initialize();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            SplitAndReplace();
         }
 
         private void textBoxShowPath_TextChanged(object sender, EventArgs e)
